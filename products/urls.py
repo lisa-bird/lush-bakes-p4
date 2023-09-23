5b5d0091e5bc814b3fc72eb0caef6b2a5fbbd5e7
lisa-bird <>
2023-09-23 19:09:33 +0000

    added product and product description views and templates

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products, name='products')
+    path('', views.products, name='products'),
+    path('<product_id>', views.product_description, name='product_description'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -12,3 +12,15 @@ def products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_description(request, product_id):
+    ''' A view to show description of product/bake '''
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_description.html', context)
