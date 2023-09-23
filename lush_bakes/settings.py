2629d170e0d16d06016d1fb8d069e3e3ea15a3c0
lisa-bird <>
2023-09-23 18:28:41 +0000

    added templates/products/products.html, styled cards

--- lush_bakes/settings.py
+++ lush_bakes/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--+=yukvwe4#5^rlj42tudl%ro^ysj_0g1*-7^uv%t2t3rwnfe-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8004-lisa-bird-lush-bakes-p4-qc919zl7kh.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['8000-lisa-bird-lush-bakes-p4-qc919zl7kh.us2.codeanyapp.com']
 
 
 # Application definition
