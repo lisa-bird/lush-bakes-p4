094d0f828db58155ed2083972ff3af0c11c6aceb
lisa-bird <>
2023-09-24 16:03:45 +0000

    fixed bug, changed basket to bag to eleviate the error

--- bag/apps.py
+++ bag/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BasketConfig(AppConfig):
+class BagConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'basket'
+    name = 'bag'
--- lush_bakes/settings.py
+++ lush_bakes/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
-    'basket',
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -73,7 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required for allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'basket.contexts.bag_contents',
+                'bag.contexts.bag_contents',
             ],
         },
     },
