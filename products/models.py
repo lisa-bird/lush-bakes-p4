87209c8cda48c6499b9d2c6806d6fd6c7bf619e9
lisa-bird <>
2023-09-24 10:46:34 +0000

    added sort categories to the view

--- products/models.py
+++ products/models.py
@@ -5,7 +5,6 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'Categories'
-        
     name = models.CharField(max_length=100)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -21,7 +20,8 @@ class Product(models.Model):
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)   
+    price = models.DecimalField(max_digits=6, decimal_places=2)      
+
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
