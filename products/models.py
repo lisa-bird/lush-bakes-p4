47fcfa035b3e4fdaf6a54a38e872636221d003f8
lisa-bird <>
2023-09-24 17:14:10 +0000

    added quantity_script.html to increment/decrement quantity of products

--- products/models.py
+++ products/models.py
@@ -20,8 +20,7 @@ class Product(models.Model):
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)      
-
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
