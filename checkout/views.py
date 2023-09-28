ff47cf478cca2191259a4b874d12ba241bfeed7b
lisa-bird <>
2023-09-28 19:30:26 +0000

    Added Stripe payment funtionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NpGKrAsyDTELZCwXapZ8GXsWjnLcfQLF8JyhDLBU3mWZbAwOBlvLskkejAyTL2tlxM6A1Nss3JcMjBP2zlPXc4h00jX1ORVRB',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
