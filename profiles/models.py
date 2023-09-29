996637a58537a9de43919a5c9ced1cad4fa95c92
lisa-bird <>
2023-09-29 20:32:56 +0000

    updated allauth templates and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
-class UserAccount(models.Model):
-    '''A user account for holding previouse 
-    delivery info 
-    '''
+class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
@@ -21,7 +22,11 @@ class UserAccount(models.Model):
 
 
 @receiver(post_save, sender=User)
-def create_or_update_user(sender, instance, created, **kwargs):
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
     if created:
-        UserAccount.objects.create(user=instance)
-    instance.useraccount.save()
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
