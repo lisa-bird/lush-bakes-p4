a349ca333aff0793c0f5f84d7dce61b2346f715c
lisa-bird <>
2023-10-03 05:59:03 +0000

    Removed secret key

--- lush_bakes/settings.py
+++ lush_bakes/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--+=yukvwe4#5^rlj42tudl%ro^ysj_0g1*-7^uv%t2t3rwnfe-'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['lush-bakes-p4-3fb0d4cd2c41.herokuapp.com', 'localhost']
 
